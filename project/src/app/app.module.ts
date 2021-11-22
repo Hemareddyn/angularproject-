7a73f74faf5bec653d0b36ac098fba957b82fab8
hnarayana <hnarayana@innominds.com>
2021-11-22 21:27:53 +0530

    forgotpassword.html

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    NavbarComponent
+    NavbarComponent,
+    ForgotpasswordComponent
   ],
   imports: [
     BrowserModule,
