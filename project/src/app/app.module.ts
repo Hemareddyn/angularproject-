151daa33f000f53191d64d963b9af06e5da7652d
hnarayana <hnarayana@innominds.com>
2021-11-22 21:13:27 +0530

    signup

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,9 @@ import { SignupComponent } from './components/signup/signup.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    // FormsModule
+    // ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
